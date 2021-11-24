88890b9258f89e994561e0c8d4b8cbf0d4de8cd9
Reece McGowan <reecem@sfu.ca>
2021-11-24 05:48:49 -0800

    Add file to output all amentity types for analysis

---
+++ extract_amenities.py
@@ -0,0 +1,9 @@
+import sys
+
+def main(in_directory, out_directory):
+    return None
+
+if __name__=='__main__':
+    in_directory = sys.argv[1]
+    out_directory = sys.argv[2]
+    main(in_directory, out_directory)
